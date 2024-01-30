301fa08e6b1a7bab7f2bba1912d4e80b9548c05c
Seo Jong-Pil <seojong789@naver.com>
2024-01-30 18:03:22 +0900

    REFACTOR/ 타이머 시간이 끝나기 전에 클릭할 경우, 다음 문제를 건너띄는 문제 해결

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,31 +1,69 @@
-import React from 'react';
+import React, { useState } from 'react';
 import QuestionTimer from './QuestionTimer';
 import Answers from './Answers';
+import QUESTIONS from '../questions';
 
-const Question = ({
-  onSkipAnswer,
-  questionText,
-  answers,
-  onSelectAnswer,
-  selectedAnswer,
-  answerState,
-}) => {
+// keyIdx를 key라고 사용하면 안 돼. key prop은 리액트만 사용가능하고 사용자는 사용할 수 없어.
+const Question = ({ keyIdx, onSkipAnswer, onSelectAnswer }) => {
+  const [answer, setAnswer] = useState({
+    selectedAnswer: '',
+    isCorrect: null,
+  });
+
+  let timer = 10000;
+  if (answer.selectedAnswer) {
+    timer = 1000;
+  }
+
+  // 사용자가 선택해서 정답 or 오답을 가진 상태라면 2000초 뒤에 다음 질문으로 넘어가기 떄문에, 2000으로 설정.
+  if (answer.isCorrect !== null) {
+    timer = 2000;
+  }
+
+  const handleSelectAnswer = (answer) => {
+    setAnswer({
+      selectedAnswer: answer,
+      isCorrect: null,
+    });
+
+    setTimeout(() => {
+      setAnswer({
+        selectedAnswer: answer,
+        // QUESTIONS[keyIdx].answers[0]는 현재 문제에 대한 정답을 의미함.
+        isCorrect: answer === QUESTIONS[keyIdx].answers[0],
+      });
+
+      setTimeout(() => {
+        onSelectAnswer(answer);
+      }, 2000);
+    }, 1000);
+  };
+
+  let answerState = '';
+  if (answer.selectedAnswer && answer.isCorrect !== null) {
+    answerState = answer.isCorrect ? 'correct' : 'wrong';
+  } else if (answer.selectedAnswer) {
+    answerState = 'answered';
+  }
+
   return (
     // 기존의 Quiz에 모여있던 Component를 분리해서 key를 생략하면 key를 다른 컴포넌트에 중복 사용해서 경고가 뜨는 문제를 해결할 수 있음
     // 어째서? -> Quiz에서 해당 컴포넌트(Question)를 호출할 때의 key로 지정하면 해결가능함.
     <div id="question">
       <QuestionTimer
-        // key={activeQuestionIndex}
-        timeout={10000}
-        onTimeout={onSkipAnswer}
+        // keyIdx={activeQuestionIndex}
+        key={timer}
+        timeout={timer}
+        onTimeout={answer.selectedAnswer === '' ? onSkipAnswer : null}
+        mode={answerState}
       />
-      <h2>{questionText}</h2>
+      <h2>{QUESTIONS[keyIdx].text}</h2>
       <Answers
-        // key={activeQuestionIndex}
-        answers={answers}
-        selectedAnswer={selectedAnswer}
+        // keyIdx={activeQuestionIndex}
+        answers={QUESTIONS[keyIdx].answers}
+        selectedAnswer={answer.selectedAnswer}
         answerState={answerState}
-        onSelectAnswer={onSelectAnswer}
+        onSelectAnswer={handleSelectAnswer}
       />
     </div>
   );
--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -4,48 +4,22 @@ import QUESTIONS from '../questions';
 import Question from './Question';
 
 const Quiz = () => {
-  const [answerState, setAnswerState] = useState('unanswered');
   const [userAnswers, setUserAnswers] = useState([]);
 
   // 다음 질문의 index 번호를 의미하는 변수.
   // userAnswers 상태에서 파생시켜 값을 얻을 수 있기 때문에, 별도의 상태로 만들 필요가 없음.
   // 가령, 첫 번째 문제의 경우 userAnswers.length의 값은 0이기 때문에 0에 해당하는 위치에 값을 저장하고
   // 두 번째 문제의 경우 userAnswers.length의 값은 1 -> [1]에 값 저장.
-  // 사용자가 보기를 선택하면 틀렸는지 맞았는지에 따라 색깔을 보여주고 다음 문제로 넘어가고 싶어.
-  // 따라서, 아직 보기를 선택하지 않았다면(unanswered 라면) 현재 그 상태로 그대로 있는데 (userAnswers.length)
-  // 만약 보기를 선택했다면 -> userAnswers.length의 값이 1이 증가한 상태(다음문제)인데, 이전 문제에 대해 틀렸는지 맞았는지를 보여줘야 해서 -1로 이전 문제를 유지시킴.
-  const activeQuestionIndex =
-    answerState === 'unanswered' ? userAnswers.length : userAnswers.length - 1;
+
+  const activeQuestionIndex = userAnswers.length;
 
   const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
 
-  const handleSelectAnswer = useCallback(
-    (selectedAnswer) => {
-      setAnswerState('answered');
-      setUserAnswers((prevAnswers) => {
-        return [...prevAnswers, selectedAnswer];
-      });
-
-      setTimeout(() => {
-        if (selectedAnswer === QUESTIONS[activeQuestionIndex].answers[0]) {
-          // 사용자가 선택한 답안이, 해당 문제에 대한 첫 번째 항목(정답)과 같다면 정답처리
-          setAnswerState('correct');
-        } else {
-          setAnswerState('wrong');
-        }
-
-        // 바로 위의 답안이 맞는지 틀렸는지를 확인하는 타이머가 끝난 직후에 바로 실행되는 타이머
-        setTimeout(() => {
-          // 선택한 보기가 틀렸는지 맞았는지 타이머로 보여준 뒤, 다음 문제를 위해서 다시 unanswered로 변경해줌.
-          // 위의 activeQuestionIndex = answerState === 'unanswered' ? userAnswers.length : userAnswers.length - 1; 코드에 의해
-          // userAnswers의 값은 사용자가 보기를 선택한 시점에서 이미 1증가 했으니 다음 문제를 가리키고 있음.
-          // 그래서 다시 unanswered로 만들어주면 다음 문제로 넘어가게 되는 로직.
-          setAnswerState('unanswered');
-        }, 2000);
-      }, 1000);
-    },
-    [activeQuestionIndex]
-  );
+  const handleSelectAnswer = useCallback((selectedAnswer) => {
+    setUserAnswers((prevAnswers) => {
+      return [...prevAnswers, selectedAnswer];
+    });
+  }, []);
 
   const handleSkipAnswer = useCallback(
     () => handleSelectAnswer(null),
@@ -67,11 +41,9 @@ const Quiz = () => {
        어째서? -> Quiz에서 해당 컴포넌트(Question)를 호출할 때의 key로 지정하면 해결가능함. */}
       <Question
         key={activeQuestionIndex}
+        // keyIdx를 key라고 사용하면 안 돼. key prop은 리액트만 사용가능하고 사용자는 사용할 수 없어.
+        keyIdx={activeQuestionIndex}
         onSkipAnswer={handleSkipAnswer}
-        questionText={QUESTIONS[activeQuestionIndex].text}
-        answers={QUESTIONS[activeQuestionIndex].answers}
-        answerState={answerState}
-        selectedAnswer={userAnswers[userAnswers.length - 1]}
         onSelectAnswer={handleSelectAnswer}
       />
     </div>
